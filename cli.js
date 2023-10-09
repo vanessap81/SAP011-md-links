034c5d18c049308d4595b958164839f077a926af
Vanessa Ponte <vanessaponte@gmail.com>
2023-10-09 19:53:12 -0300

    feat: imprime links e texto de arquivo md

--- cli.js
+++ cli.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
 
-console.log("Oi, CLI!");
+import chalk from "chalk";
+import { mdLinks } from "./src/index.js";
+
+// o segundo argumento digitado será capturado para análise
+const file = process.argv[2];
+
+mdLinks(file)
+    .then((results) => {
+        results.forEach((result) => {
+        console.log(chalk.bold.bgYellow(result.title), result.href);
+        });
+});
--- src/index.js
+++ src/index.js
@@ -1,50 +1,29 @@
-import { error } from "console";
 import { readFile } from "fs";
-// import { writeFile } from "fs";
 
-// writeFile("./src/example.txt", "Um breve texto aqui", function (err) {
-//   // Caso ocorra algum erro
-//   if (err) {
-//     return console.log("erro");
-//   }
+const mdLinks = (filePath) => {
+  const urlRegex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const results = [];
 
-//   console.log("Arquivo criado");
-// });
-
-// readFile("./src/example.txt", "utf-8", function (err, data) {
-//   console.log(data);
-// });
-
-// const urlRegex =
-//   /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
-
-// const urls = "https://www.freecodecamp.org";
-
-// console.log(urlRegex.test(urls));
-
-const getFile = async (fileName) => {
-  try {
-    const data = await readFile(fileName, "utf8", function (err, data) {
-      console.log(`texto do documento: ${data}`);
+  return new Promise((resolve, reject) => {
+    //Leitura do aquivo aqui
+    readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err.message);
+      } else {
+        const regex = data.match(urlRegex);
+        regex.forEach((index) => {
+          const title = index.match(/\[(\S.*)\]/)[1];
+          const href = index.match(/\((http.*)\)/)[1];
+          results.push({ title, href });
+        });
+        
+    //Validação dos links
+    
+      resolve(results);
+      }
     });
-  } catch (error) {
-    console.log(error);
-  }
+  });
+
 };
 
-const textDoc = getFile("./src/example.txt");
-
-// padrão para link em .md
-// const regex = /\\[([^\\]]*)\\]\\((https?:\\/\\/[^$#\\s].[^\\s]*)\\)/gm
-
-const findLinks = (text) => {
-  const urlRegex =
-    /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
-
-  // const links = [];
-  const oneLink = urlRegex.exec(text);
-
-  console.log(`link do documento: ${oneLink}`);
-};
-
-findLinks(textDoc);
+export { mdLinks };
